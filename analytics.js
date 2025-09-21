59b1bd0943dd95b748cffa74b38d1ac6cb371345
Cem Sümengen <cemsumengen@gmail.com>
2025-09-21 21:50:52 +0300

    modified:   analytics.js 	new file:   ecommerce_case_study/app-interface-transparent.png 	new file:   ecommerce_case_study/app_logo.png 	new file:   ecommerce_case_study/dolap.webp 	new file:   ecommerce_case_study/ecommerce_case_presentation.html 	new file:   ecommerce_case_study/favicon.png 	new file:   ecommerce_case_study/gardrops.webp 	new file:   ecommerce_case_study/letgo.webp 	modified:   index.html 	modified:   resume.html
        Added second-hand startup case study.

--- analytics.js
+++ analytics.js
@@ -1,13 +1,34 @@
 // analytics.js
-(function() {
-  var gtagScript = document.createElement('script');
-  gtagScript.async = true;
-  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-ZEGH58921N';
-  document.head.appendChild(gtagScript);
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
 
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  window.gtag = gtag;
+// Load Google Analytics
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-ZEGH58921N';
+  document.head.appendChild(script);
+
   gtag('js', new Date());
-  gtag('config', 'G-ZEGH58921N');
+
+  // Dynamic tracking based on current page
+  const path = window.location.pathname;
+
+  if (path === '/' || path === '/index.html') {
+    gtag('config', 'G-ZEGH58921N', {
+      page_title: 'Home',
+      page_path: '/index.html'
+    });
+  } else if (path === '/resume.html') {
+    gtag('config', 'G-ZEGH58921N', {
+      page_title: 'Resume',
+      page_path: '/resume.html'
+    });
+  } else {
+    // Fallback for any future pages
+    gtag('config', 'G-ZEGH58921N', {
+      page_title: document.title,
+      page_path: path
+    });
+  }
 })();
